eb2015cf01f42e96e196f1ca4a9a84d6be5003e8
saka-21 <kasaikasai572@gmail.com>
2020-11-27 21:37:01 +0900

    1127

--- typing.js
+++ typing.js
@@ -76,3 +76,36 @@ function finish() {
   random = 0;
   char_num = 0;
 }
+
+document.onkeydown = function (e) {
+  if (e.keyCode == 189) {
+    keyStr = "-";
+  } else if (e.keyCode == 188) {
+    keyStr = ","
+  } else {
+    var keyStr = String.fromCharCode(e.keyCode);
+    keyStr = keyStr.toLowerCase();
+  }
+  if (keyStr == word_char) {
+    document.getElementById('missaudio').pause();
+    document.getElementById('missaudio').currentTime = 0;
+    document.getElementById('correctaudio').pause();
+    document.getElementById('correctaudio').currentTime = 0;
+    document.getElementById('correctaudio').play();
+    word.innerHTML = "<span style='color: red;'>" + wordlist[random].substr(0, char_num + 1) + "</span>" + wordlist[random].substr(char_num + 1, wordlist[random].length);
+    char_num++;
+    correct++;
+    charInsort();
+  } else {
+    document.getElementById('missaudio').pause();
+    document.getElementById('missaudio').currentTime = 0;
+    document.getElementById('correctaudio').pause();
+    document.getElementById('correctaudio').currentTime = 0;
+    mistake++;
+    document.getElementById('missaudio').play();
+  }
+  if (char_num == wordlist[random].length) {
+    char_num = 0;
+    wordDisplay();
+  }
+};
